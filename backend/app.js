e673ecf27631b14d2c1f822641c1e07eee0edcf5
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2022-08-27 23:51:55 +0100

    the email confirmation is finally working just remaining to use sendgrid for the contact and remove completly the outlook mail will do that next time

--- backend/app.js
+++ backend/app.js
@@ -1,68 +1,58 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
 
-const transport = nodemailer.createTransport({
-  service: "hotmail",
-  auth: {
-    user: process.env.MY_MAIL,
-    pass: process.env.MY_PASSWORD,
-  },
-});
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
 
-app.post("/approve", cors(), (req, res) => {
+app.post('/approve', cors(), (req, res) => {
   let { useremail } = req.body;
 
-  transport.sendMail(
-    {
-      from: process.env.MY_MAIL,
+  sgMail
+    .send({
       to: useremail,
-      subject: "Approved",
-      text: "Great you can come to perfitcom for us to train you.",
-    },
-    (err, info) => {
-      if (err) {
-        console.log(err);
-        res.status(400).send("bad job");
-      } else {
-        console.log(info.response);
-        res.status(200).send("good job");
-      }
-    }
-  );
+      from: 'ngamessidebora89@gmail.com',
+      subject: 'Approved',
+      text: 'Great you can come to perfitcom for us to train you.',
+    })
+    .then(() => {
+      console.log('Email sent');
+      res.status(200).send('good job');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send('bad job');
+    });
 });
 
-app.post("/reject", cors(), (req, res) => {
+app.post('/reject', cors(), (req, res) => {
   let { useremail } = req.body;
 
-  transport.sendMail(
-    {
-      from: process.env.MY_MAIL,
+  sgMail
+    .send({
       to: useremail,
-      subject: "Regestion",
-      text: "Sorry you have been refused.",
-    },
-    (err, info) => {
-      if (err) {
-        console.log(err);
-        res.status(400).send("bad job");
-      } else {
-        console.log(info.response);
-        res.status(200).send("good job");
-      }
-    }
-  );
+      from: 'ngamessidebora89@gmail.com',
+      subject: 'Regection',
+      text: 'Sorry you have been refused.',
+    })
+    .then(() => {
+      console.log('Email sent');
+      res.status(200).send('good job');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send('bad job');
+    });
 });
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log("Server is listening on port 4000");
+  console.log('Server is listening on port 4000');
 });
